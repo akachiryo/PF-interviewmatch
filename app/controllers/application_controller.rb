c5a43aec9c53fd02970c1fe1a3127c0ddabdcbc5
akachiryo <rirugameri@icloud.com>
2021-09-08 17:36:50 +0000

    top-layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
-  
+
  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ def after_sign_out_path_for(resource)
+
+    root_path(resource)
+  end
+
+
 
 private
 
@@ -15,16 +22,7 @@ private
     end
   end
 
-  def after_sign_out_path_for(resource)
-    case resource
-      when Admin
-        flash[:notice] = "adminログアウトしました"
-        root_path
-      when User
-        flash[:notice] = "ログアウトしました"
-        root_path
-    end
-  end
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
-  devise_for :admins
   devise_for :users
+  devise_for :admins
+
 
   namespace :users do
     get "rooms/search" => "rooms#search"
