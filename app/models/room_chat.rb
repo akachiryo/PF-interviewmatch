4b7729adcddc753a8895a83e3f95644447450fb8
akachiryo <rirugameri@icloud.com>
2021-09-21 14:11:36 +0000

    room_chat

--- app/models/room_chat.rb
+++ app/models/room_chat.rb
@@ -1,8 +1,19 @@
 class RoomChat < ApplicationRecord
 
   has_many :notifications, dependent: :destroy
-  
+
   belongs_to :user
   belongs_to :room
-  
+
+  def self.create_notification_room_chat(current_user, user, room_chat)
+    temp = Notification.where(visiter_id: current_user.id, visited_id: user.id, room_chat_id: room_chat.id, action: "room_chat")
+    if temp.blank?
+      notification = Notification.new(visiter_id: current_user.id, visited_id: user.id, room_chat_id: room_chat.id, action: "room_chat")
+      if notification.visiter_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
+
 end
