51a06e6b89f791e7a369bc4cda278cb05cd43d1b
akachiryo <rirugameri@icloud.com>
2021-09-25 13:02:47 +0000

    topページ追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @rooms = Room.includes([:tags], [:user]).page(params[:page]).per(10).order(created_at: :desc)
+    @rooms = Room.includes([:tags], [:user]).page(params[:page]).per(3).order(created_at: :desc)
     @tags = Tag.joins(:room_tags).group(:tag_id).order('count(tag_id) desc').limit(10)
   end
 
