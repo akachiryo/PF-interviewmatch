38637fa85290a575914ecdddf98349b21991d7d5
akachiryo <rirugameri@icloud.com>
2021-09-28 11:13:29 +0000

    完成　調整

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,8 @@ class Tag < ApplicationRecord
   has_many :room_tags, dependent: :destroy, foreign_key: 'tag_id'
   has_many :rooms, through: :room_tags
 
+  validates :name, presence: true
+
   def self.search(keyword)
     where(["name like?", "%#{keyword}%"])
   end
--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe "Homes", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET #about" do
+    it "リクエストが成功すること" do
+      get homes_about_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
